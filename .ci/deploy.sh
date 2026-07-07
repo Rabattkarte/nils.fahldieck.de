1967598aeb7e3d026239b903b73068d4cabbbf84
Nils Fahldieck <nils@fahldieck.de>
2026-07-07 15:07:20 +0200

    Remove old ci script

--- .ci/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [ -z ${deploy_host+x} ]; then echo "E: Variable 'deploy_host' is unset" && exit 1; fi
-if [ -z ${deploy_dest+x} ]; then echo "E: Variable 'deploy_dest' is unset" && exit 1; fi
-
-hugo --cleanDestinationDir && rsync -avz --delete public/ "$deploy_host:$deploy_dest"
-
-exit 0
